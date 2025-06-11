5d8211cb0e63f7ec6b751f81b3821ddc99b152d8
Andrew <andrew123709@gmail.com>
2025-06-10 22:45:46 -0400

    Change back main page from testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import WaitingRoom from "@/components/WaitingRoom";
+import UserBanner from "@/components/UserBanner";
 
 export default function Home() {
   return (
-    <WaitingRoom teamId="123" />
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">UW DSC Estimathon</h1>
+      <UserBanner />
+    </main>
   );
 }
