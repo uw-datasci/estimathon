dc2dba8f117c3f99813ec5a237ec83aaed495535
Andrew <andrew123709@gmail.com>
2025-06-11 00:26:09 -0400

    revert temp main page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Backdrop from "@/components/Backdrop";
 import UserBanner from "@/components/UserBanner";
-import WaitingRoom from "@/components/WaitingRoom";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-4">
-      <WaitingRoom/>
+      <Backdrop/>
+      <h1 className="text-3xl font-bold">UW DSC Estimathon</h1>
+      <UserBanner />
     </main>
   );
 }
