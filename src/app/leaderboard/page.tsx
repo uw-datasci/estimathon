6e89d23686a61a7985220d60c1337809e8afbe45
Andrew <andrew123709@gmail.com>
2025-07-23 18:28:35 -0400

    timestamp fix

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { redirect } from "next/navigation";
 import { supabaseAdmin } from "@/lib/supabase";
 import LeaderboardClient from "./LeaderboardClient";
